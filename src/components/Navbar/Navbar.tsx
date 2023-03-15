3dc331e0534343ed19c77c6567159e9ebc175dbe
Edward Do <edwardtdo@gmail.com>
2023-03-15 18:57:32 +1100

    firebase configuration

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,15 +5,8 @@ import ViewSidebarOutlinedIcon from '@mui/icons-material/ViewSidebarOutlined';
 
 import { useDispatch } from "react-redux";
 import { toggleSidebar } from "@/store/appSlice";
-import { useSelector } from "react-redux";
-import { RootState } from "@/store/store";
-
-interface NavbarProps {
-	// ...	
-}
 
 export default function Navbar () {
-	const showSidebar = useSelector((state: RootState) => state.app.showSidebar);
 	const dispatch = useDispatch();
 
 	return (
